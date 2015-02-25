e33c3de1bb919a891bd3b5598ac442b40015c694
Baptiste Wicht <baptiste.wicht@gmail.com>
2015-02-25 10:02:40 +0100

    Keep only v2

--- v2/include/normalize.h
+++
@@ -1,6 +0,0 @@
-#include "../include/model.h"
-
-#ifndef NORMALIZE_H_INCLUDED
-#define NORMALIZE_H_INCLUDED
-void normalize(model_t *model, double** Test_Vect, int pos);
-#endif
--- v2/include/test_matrix.h
+++
@@ -1,8 +0,0 @@
-#ifndef TESTMATRIX_H_INCLUDED
-#define TESTMATRIX_H_INCLUDED
-
-#include "model.h"
-
-int test_matrix(model_t* foo_mat, double **Test_Vect, int T, int NClasses, int *Align, double *Pvit_w);
-
-#endif
